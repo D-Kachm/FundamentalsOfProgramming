87560135b075a3845ac35774e30bcda5ffb0f06e
D-Kachm <Denys.Kachmar@lnu.edu.ua>
2024-10-10 10:50:32 +0300

    change files

--- LaboratoryWork5/numbers_in_words/main.c
+++ LaboratoryWork5/numbers_in_words/main.c
@@ -1,49 +1,58 @@
 #include <stdio.h>
 
-// Функція для виведення числа від 1 до 19 прописом
-void print_1_to_19(int n) {
-    char *words[] = {"", "один", "два", "три", "чотири", "п'ять", "шість", "сім", "вісім", "дев'ять", 
-                     "десять", "одинадцять", "дванадцять", "тринадцять", "чотирнадцять", 
-                     "п'ятнадцять", "шістнадцять", "сімнадцять", "вісімнадцять", "дев'ятнадцять"};
-    printf("%s", words[n]);
-}
-
-// Функція для виведення десятків прописом
-void print_tens(int n) {
-    char *words[] = {"", "", "двадцять", "тридцять", "сорок", "п'ятдесят", "шістдесят", 
-                     "сімдесят", "вісімдесят", "дев'яносто"};
-    printf("%s", words[n]);
-}
-
-// Функція для виведення числа сотень прописом
-void print_hundreds(int n) {
-    char *words[] = {"", "сто", "двісті", "триста", "чотириста", "п'ятсот", "шістсот", 
-                     "сімсот", "вісімсот", "дев'ятсот"};
-    printf("%s", words[n]);
-}
-
-void number_to_words(int num) {
-    if (num == 0) {
-        printf("нуль");
-        return;
+void printHundreds(int digit) {
+    switch (digit) {
+        case 1: printf("сто "); break;
+        case 2: printf("двісті "); break;
+        case 3: printf("триста "); break;
+        case 4: printf("чотириста "); break;
+        case 5: printf("п'ятсот "); break;
+        case 6: printf("шістсот "); break;
+        case 7: printf("сімсот "); break;
+        case 8: printf("вісімсот "); break;
+        case 9: printf("дев'ятсот "); break;
     }
+}
 
-    if (num >= 100) {
-        print_hundreds(num / 100);
-        num %= 100;
-        if (num > 0) {
-            printf(" ");
-        }
+void printTens(int digit) {
+    switch (digit) {
+        case 2: printf("двадцять "); break;
+        case 3: printf("тридцять "); break;
+        case 4: printf("сорок "); break;
+        case 5: printf("п'ятдесят "); break;
+        case 6: printf("шістдесят "); break;
+        case 7: printf("сімдесят "); break;
+        case 8: printf("вісімдесят "); break;
+        case 9: printf("дев'яносто "); break;
     }
+}
 
-    if (num >= 20) {
-        print_tens(num / 10);
-        if (num % 10 != 0) {
-            printf(" ");
-            print_1_to_19(num % 10);
-        }
-    } else if (num > 0) {
-        print_1_to_19(num);
+void printTeens(int digit) {
+    switch (digit) {
+        case 0: printf("десять "); break;
+        case 1: printf("одинадцять "); break;
+        case 2: printf("дванадцять "); break;
+        case 3: printf("тринадцять "); break;
+        case 4: printf("чотирнадцять "); break;
+        case 5: printf("п'ятнадцять "); break;
+        case 6: printf("шістнадцять "); break;
+        case 7: printf("сімнадцять "); break;
+        case 8: printf("вісімнадцять "); break;
+        case 9: printf("дев'ятнадцять "); break;
+    }
+}
+
+void printUnits(int digit) {
+    switch (digit) {
+        case 1: printf("один "); break;
+        case 2: printf("два "); break;
+        case 3: printf("три "); break;
+        case 4: printf("чотири "); break;
+        case 5: printf("п'ять "); break;
+        case 6: printf("шість "); break;
+        case 7: printf("сім "); break;
+        case 8: printf("вісім "); break;
+        case 9: printf("дев'ять "); break;
     }
 }
 
@@ -54,12 +63,24 @@ int main() {
     scanf("%d", &number);
 
     if (number < 100 || number > 999) {
-        printf("Будь ласка, введіть тризначне число.\n");
-    } else {
-        printf("Число прописом: ");
-        number_to_words(number);
-        printf("\n");
+        printf("Помилка: введіть тризначне число.\n");
+        return 1;
     }
 
+    int hundreds = number / 100;
+    int tens = (number / 10) % 10;
+    int units = number % 10;
+
+    printHundreds(hundreds);
+
+    if (tens == 1) {
+        printTeens(units); // Якщо число від 10 до 19
+    } else {
+        printTens(tens);   // Якщо десятки звичайні (від 20 до 90)
+        printUnits(units); // Друк одиниць
+    }
+
+    printf("\n");
     return 0;
 }
+
